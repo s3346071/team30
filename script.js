84c8944ce168d6b0f6ea29de624cbe27bcd59d78
Yury Antropov <s3346071@rmit.edu.au>
2020-10-16 20:19:48 +1100

    css + js

--- script.js
+++ script.js
@@ -213,14 +213,21 @@ textbox.addEventListener('click', e=> {
   textbox.classList.remove('active')
 })
 
-var yuryinfo = document.getElementByClassName("yury_info")
 
-// yuryinfo.addEventListener("click",
-//   function() {
-//     infobox.innerHTML =
-//     `
-//     <p>Here is Yury's profile</p>
-//
-//     `
-//   }
-// )
+
+
+function animateOnScroll(){
+  let target = document.querySelectorAll(".fade-in");
+  target.forEach(i=>{
+    let targetTop = i.getBoundingClientRect().top;
+    let windowHeight = window.innerHeight;
+    if (targetTop <= windowHeight * 0.40) {
+      i.classList.add("appear");
+    }
+     else if (targetTop >= windowHeight*0.10) {
+       i.classList.remove("appear");
+     }
+  })
+}
+
+window.addEventListener('scroll', animateOnScroll)
